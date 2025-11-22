c7738c7acbd491a27b019b701bcd6a8a9ffc225b
Yana07-Chaikovska <chaikovska_ak24@nuwm.edu.ua>
2025-11-23 00:27:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace RegexIpFinder
 {
@@ -7,10 +9,9 @@ namespace RegexIpFinder
         static void Main()
         {
             Console.WriteLine("Введіть текст для пошуку IP-адрес:");
-            string inputText = Console.ReadLine();
+            string inputText = Console.ReadLine() ?? string.Empty;
 
-            IpFinder finder = new IpFinder();
-            var ipAddresses = finder.FindIpAddresses(inputText);
+            List<string> ipAddresses = FindIpAddresses(inputText);
 
             if (ipAddresses.Count == 0)
             {
@@ -19,11 +20,29 @@ namespace RegexIpFinder
             else
             {
                 Console.WriteLine("Знайдені IP-адреси:");
-                foreach (var ip in ipAddresses)
+                foreach (string ip in ipAddresses)
                 {
                     Console.WriteLine(ip);
                 }
             }
+        }
+
+        private static List<string> FindIpAddresses(string text)
+        {
+            const string ipPattern =
+                @"\b((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}"
+                + @"(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\b";
+
+            List<string> results = new List<string>();
+
+            MatchCollection matches = Regex.Matches(text, ipPattern);
+
+            foreach (Match match in matches)
+            {
+                results.Add(match.Value);
+            }
+
+            return results;
         }
     }
 }
